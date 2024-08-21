1748c47f05e2a29a286b00e18aa6a5af29351192
Miguel <miguel.rodriguez.valencia@gmail.com>
2024-08-21 18:30:59 +0200

    Final push

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://pedrovelasquez9.github.io",
-    base: "/astro-test",
+    site: "https://miguelrv12.github.io",
+    base: "/AstroProject",
     i18n: {
         defaultLocale: "es",
         locales: ["es", "en"],
